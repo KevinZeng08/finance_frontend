1922acb82a2268cc070bcb45263437add54f03ec
KevinZeng <2538015266@qq.com>
2023-06-19 15:45:53 +0800

    Test Finish

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,10 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import Hello from '@/components/Hello.vue'
 
 export default {
   name: 'App',
   components: {
-    Hello
   }
 }
 </script>
