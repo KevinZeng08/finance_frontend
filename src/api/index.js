2bcceccadc21e0d89d063f7708b30e97da036f28
SuzyWan0915 <847107328@qq.com>
2023-06-23 14:24:59 +0800

    state

--- src/api/index.js
+++ src/api/index.js
@@ -69,6 +69,9 @@ export const reqAddFund = (query) => {
   return requests({
     url: "/admin/fund/add/",
     method: "POST",
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
     data: query,
   })
 }
